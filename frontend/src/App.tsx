6fa41472d64ce16c46a076ae48c937329e0c8891
Anubic29 <asitailo2039@gmail.com>
2024-03-01 12:33:39 +0200

    init: create initial react-ts app with scss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import styles from './App.module.scss'
+
+function App() {
+  return (
+    <div className={styles['block']}>
+      <h1 className={styles['title']}>
+        FiestaFood
+      </h1>
+    </div>
+  )
+}
+
+export default App
