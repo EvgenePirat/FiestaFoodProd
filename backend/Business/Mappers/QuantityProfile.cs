217c4fd562840c489711bb01c027171c9aa99b79
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-03-20 23:11:39 -0700

    #1.12 Create mapper for working with ingredient and quantity

---
+++ backend/Business/Mappers/QuantityProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Business.Models.Quantity.Request;
+using Business.Models.Quantity.Response;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Mappers
+{
+    public class QuantityProfile : Profile
+    {
+        public QuantityProfile()
+        {
+            CreateMap<CreateQuantityModel, Quantity>();
+            CreateMap<UpdateQuantityModel, Quantity>();
+            CreateMap<Quantity, QuantityModel>();
+        }
+    }
+}
