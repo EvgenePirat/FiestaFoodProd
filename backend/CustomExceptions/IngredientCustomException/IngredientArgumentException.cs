822ab28b1e3f8e6a7e4e042397eff1ebc84eefa9
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-03-20 23:09:21 -0700

    #1.10 Create custom ingridient exception and fix don`t need exception

---
+++ backend/CustomExceptions/IngredientCustomException/IngredientArgumentException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptions.IngredientCustomException
+{
+    public class IngredientArgumentException : Exception
+    {
+        public IngredientArgumentException() : base() { }
+
+        public IngredientArgumentException(string message) : base(message) { }
+
+        public IngredientArgumentException(string? message, Exception? innerException) : base(message, innerException) { }
+    }
+}
