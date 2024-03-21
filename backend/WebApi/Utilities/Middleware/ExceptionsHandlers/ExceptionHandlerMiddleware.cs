54350bbc4531130379766d488ede2cbc493aba91
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2024-03-20 23:15:54 -0700

    #1.16 Registration exception in meddleware

--- backend/WebApi/Utilities/Middleware/ExceptionsHandlers/ExceptionHandlerMiddleware.cs
+++ backend/WebApi/Utilities/Middleware/ExceptionsHandlers/ExceptionHandlerMiddleware.cs
@@ -1,5 +1,5 @@
 ﻿using System.Net;
-using CustomExceptions.CustomerInfoCustomException;
+using CustomExceptions.IngredientCustomException;
 using CustomExceptions.OrderCustomExceptions;
 using CustomExceptions.OrderDetailCustomExceptions;
 using CustomExceptions.UserCustomException;
@@ -38,8 +38,8 @@ namespace WebApi.Utilities.Middleware.ExceptionsHandlers
                 ApplicationException _ => new ExceptionResponse(HttpStatusCode.BadRequest, "Application exception occurred."),
                 KeyNotFoundException _ => new ExceptionResponse(HttpStatusCode.NotFound, "The request key not found."),
                 UnauthorizedAccessException _ => new ExceptionResponse(HttpStatusCode.Unauthorized, "Unauthorized."),
-                CustomerInfoArgumentException _ => new ExceptionResponse(HttpStatusCode.Conflict, exception.Message),
                 UserArgumentException _ => new ExceptionResponse(HttpStatusCode.Conflict, exception.Message),
+                IngredientArgumentException _ => new ExceptionResponse(HttpStatusCode.BadRequest, exception.Message),
                 OperationCanceledException _ => new ExceptionResponse(HttpStatusCode.RequestTimeout, exception.Message),
                 OrderArgumentException _ => new ExceptionResponse(HttpStatusCode.RequestTimeout, exception.Message),
                 OrderDetailArgumentException _ => new ExceptionResponse(HttpStatusCode.RequestTimeout, exception.Message),
