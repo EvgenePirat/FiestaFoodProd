e43a8f0c295afd955afdae73771e77f945aaa837
Ihor <81475109+stilllone@users.noreply.github.com>
2024-03-29 14:38:49 +0200

    Configuring Cors, adding SignalR connectio

--- backend/WebApi/Program.cs
+++ backend/WebApi/Program.cs
@@ -4,10 +4,12 @@ using Business.DependencyResolvers;
 using DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
+using RealTimeHandler.Hubs;
 using WebApi.DependencyResolve;
 using WebApi.Utilities.Filters.CancellationFilter;
 using WebApi.Utilities.Filters.FormFileFilter;
 using WebApi.Utilities.Middleware.ExceptionsHandlers;
+using Autofac.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,11 +34,32 @@ var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User=SA;P
 //var connectionString = builder.Configuration.GetConnectionString("StDatabase");
 builder.Services.AddDbContext<StContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddCors();
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
+    cors =>
+    {
+        cors.AllowAnyHeader()
+            .AllowAnyMethod()
+            .SetIsOriginAllowed((_) => true)
+            .AllowCredentials();
+    }));
+
+builder.Services.AddCors(o =>
+{
+    o.AddPolicy("AllowAnyOrigin", p => p
+        .WithOrigins("null") // Origin of an html file opened in a browser
+        .AllowAnyHeader()
+        .AllowCredentials());
+});
+builder.Services.AddSignalR();
 
 builder.Services.AddControllers()
     .AddJsonOptions(opt => { opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); });
-
+builder.Services.AddSignalR()
+    .AddHubOptions<OrderHub>(options => options.EnableDetailedErrors = true)
+    .AddJsonProtocol(options =>
+    {
+        options.PayloadSerializerOptions.PropertyNamingPolicy = null;
+    }); ;
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 
 builder.Host.ConfigureContainer<ContainerBuilder>(
@@ -61,9 +84,12 @@ app.UseMiddleware<ExceptionHandlerMiddleware>();
 
 //app.UseHttpsRedirection();
 
-app.UseCors(o => o.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
-app.UseAuthorization();
+//app.UseCors("CorsPolicy");
+app.UseCors("AllowAnyOrigin");
+app.MapHub<OrderHub>("/orderHub");
 
+
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
